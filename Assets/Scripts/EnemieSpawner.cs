acc4426e08ea8092dd6a47be8d02696d8adb4a87
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-31 08:09:00 -0300

    enemie spawner

--- Assets/Scripts/EnemieSpawner.cs
+++ Assets/Scripts/EnemieSpawner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemieSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Enemie[] enemiePrototypes = null;
+    [SerializeField] private float minTimeBetweenSpawns = 0.0f;
+    [SerializeField] private float maxTimeBetweenSpawns = 0.0f;
+
+    [SerializeField] private Transform[] spawnPoints = null;
+
+    private float timeToSpawn;
+
+    private void Update()
     {
-        
+        if (Time.time > timeToSpawn){
+            Instantiate
+                (
+                    original:
+                    enemiePrototypes[GetRandomIndex(max: enemiePrototypes.Length)],
+                    
+                    position:
+                    spawnPoints[GetRandomIndex(max: spawnPoints.Length)].position,
+
+                    rotation:
+                    Quaternion.identity
+                );
+
+            float cooldownToSpawn = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+            timeToSpawn = Time.time + cooldownToSpawn;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private int GetRandomIndex(int max)
     {
-        
+        return Random.Range(minInclusive: 0, maxExclusive: max);
     }
 }
