9d40133967898ed1fc8facddd83091ed0f32ed16
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-31 07:38:08 -0300

    Get rid of bulletfactory

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class AppleTree : Tree
 {
+    [SerializeField] private Bullet applePrototype = null;
+
+    private TransformWrapper transformWrapper = null;
+
+    private void Awake()
+    {
+        transformWrapper = new TransformWrapper(transform);
+    }
+
     private void Update()
     {
         // Shoot if needed
         if (IsTimeToShoot() && IsThereEnemieInRange())
         {
-            Enemie closestEnemie = GetClosestEnemie();
-            BulletFactory.CreateAndShootApple(BulletSpawnPoint, closestEnemie.TransformWrapper);
+            Vector3 spawnPosition = transformWrapper.Position;
+            Vector3 enemyPosition = GetClosestEnemie().TransformWrapper.Position;
+
+            // Instantiate Apple
+            Bullet apple = Instantiate(applePrototype, spawnPosition, Quaternion.identity);
+            Vector3 targetDirection = (enemyPosition - spawnPosition).normalized;
+            apple.RB2D.velocity = apple.Speed * targetDirection;
+
             MarkShootInTimer();
         }
     }
