f40ace473995aae8a00d7f051f4d9c1d5df3458a
Caina Correa Caldas <cainacc@hotmail.com>
2021-10-31 16:16:12 -0300

    Waves system

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -4,31 +4,78 @@ using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour
 {
-    [SerializeField] private Enemy[] enemiePrototypes = null;
-    [SerializeField] private float minTimeBetweenSpawns = 0.0f;
-    [SerializeField] private float maxTimeBetweenSpawns = 0.0f;
+    [System.Serializable]
+    private struct WaveData
+    {
+        [SerializeField] private List<Enemy> enemiesToClone;
+        [SerializeField] private float minTimeBetweenSpawns;
+        [SerializeField] private float maxTimeBetweenSpawns;
+
+        public List<Enemy> EnemiesToSpawn { get => enemiesToClone; }
+        public float MinTimeBetweenSpawns { get => minTimeBetweenSpawns; }
+        public float MaxTimeBetweenSpawns { get => maxTimeBetweenSpawns; }
+    }
 
     [SerializeField] private Transform[] spawnPoints = null;
+
+    [SerializeField] private WaveData[] waves = null;
+    private int currentWaveIndex;
+
+    private List<Enemy> aliveEnemies = new List<Enemy>();
 
     private float timeToSpawn;
 
     private void Update()
     {
-        if (Time.time > timeToSpawn){
-            Instantiate
-                (
-                    original:
-                    enemiePrototypes[GetRandomIndex(max: enemiePrototypes.Length)],
-                    
-                    position:
-                    spawnPoints[GetRandomIndex(max: spawnPoints.Length)].position,
+        if (currentWaveIndex < waves.Length)
+        {
+            WaveData currentWaveData = waves[currentWaveIndex];
 
-                    rotation:
-                    Quaternion.identity
-                );
+            if (Time.time > timeToSpawn)
+            {
+                // Instantiate prototype and remove it from list if needed
+                if (currentWaveData.EnemiesToSpawn.Count > 0)
+                {
+                    Enemy enemyToClone = currentWaveData.EnemiesToSpawn[0];
+                    currentWaveData.EnemiesToSpawn.RemoveAt(0);
 
-            float cooldownToSpawn = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
-            timeToSpawn = Time.time + cooldownToSpawn;
+                    Vector3 spawnPosition =
+                        spawnPoints[GetRandomIndex(max: spawnPoints.Length)].position;
+
+                    Enemy enemy =
+                        Instantiate(enemyToClone, spawnPosition, Quaternion.identity);
+                    aliveEnemies.Add(enemy);
+                }
+
+                // Set new timeToSpawn based on cooldown from currentWaveData
+                float cooldownToSpawn = Random.Range
+                    (currentWaveData.MinTimeBetweenSpawns, currentWaveData.MaxTimeBetweenSpawns);
+                timeToSpawn = Time.time + cooldownToSpawn;
+            }
+
+            bool allEnemiesOfThisWaveGotKilled =
+                aliveEnemies.Count == 0 && currentWaveData.EnemiesToSpawn.Count == 0;
+            if (allEnemiesOfThisWaveGotKilled)
+            {
+                // Start next wave
+                currentWaveIndex++;
+            }
+            else
+            {
+                for (int e = aliveEnemies.Count - 1; e >= 0; e--)
+                {
+                    Enemy enemy = aliveEnemies[e];
+                    bool enemyIsDead = enemy == null;
+                    if (enemyIsDead)
+                    {
+                        aliveEnemies.RemoveAt(e);
+                    }
+                }
+            }
+        }
+        else
+        {
+            // TODO: Tell the player he won!
         }
     }
 
